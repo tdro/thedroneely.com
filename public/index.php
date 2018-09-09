cd89cf8181e175d26f9a0ce804ccf92762238e3b
Thedro Neely <thedroneely@gmail.com>
2018-09-09 04:15:23 -0400

    vendor/public: Hugo build run to update public static files

--- public/index.php
+++ public/index.php
@@ -6,6 +6,9 @@ require '../bootstrap/Bootstrap.php';
 /* create new router */
 $router = new Router;
 
+/* instantiate navigator functions */
+$navigation = new Navigation();
+
 /* direct routes as defined */
 require Router::load('../app/Routes.php')
     ->direct(Request::uri(), Request::method());
