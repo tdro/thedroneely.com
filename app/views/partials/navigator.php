f62388125bcc2d7853663af6cd8d7a8fa9b7856f
Thedro Neely <thedroneely@gmail.com>
2018-11-08 00:02:20 -0500

    app/views/partials/navigator: Add IDs to navigation bars

--- app/views/partials/navigator.php
+++ app/views/partials/navigator.php
@@ -1,32 +1,54 @@
-    <nav class="navbar navbar__custom is-fixed-top" aria-label="main navigation">
+    <nav id="navbar" class="navbar navbar__custom is-fixed-top" aria-label="main navigation">
         <div class="container">
+
             <div class="navbar-brand">
 
-                <?php $navigation->generatehomeButton(); ?>
+                <a href="/" class="has-text-white">
+                    <div class="navbar__mobile navbar__custom has-text-weight-semibold navbar-item">Home</div>
+                </a>
 
-                <div class="navbar-burger burger has-text-white" data-target="navMenu">
+                <div id="navBurger" class="navbar-burger burger has-text-white" data-target="navMenu">
                   <span></span>
                   <span></span>
                   <span></span>
-                </div>
-            </div>
-            <div class="navbar-menu" id="navMenu">
-
-            <div class="navbar-start">
-
-                <div class="navbar-item">
-                <div class="control">
-                  <input class="navbar__search input" type="text">
-                </div>
                 </div>
 
             </div>
+
+            <div id="navMenu" class="navbar__mobile navbar-menu">
 
                 <div class="navbar-end">
+                    <?php $navigation->generatehomeButton(); ?>
+                    <?php $navigation->generateNavBar(); ?>
+                </div>
 
-                <?php $navigation->generateNavBar(); ?>
+            </div>
+
+            <div class="navbar__desktop navbar-menu">
+
+                <!-- <div class="navbar-start">
+
+                    <div class="navbar-item">
+                    <div class="control">
+                      <input class="navbar__search input" type="text">
+                    </div>
+                    </div>
+
+                </div> -->
+
+                <div class="columns is-centered heading is-size-7">
+
+                    <div class="navbar__left column is-3">
+                        <?php $navigation->generatehomeButton(); ?>
+                    </div>
+
+                    <div class="navbar__right column is-5">
+                        <?php $navigation->generateNavBar(); ?>
+                    </div>
 
                 </div>
+
             </div>
+
         </div>
     </nav>
