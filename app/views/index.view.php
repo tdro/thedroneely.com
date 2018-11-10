17311074f2535b2592acdf550d8b6fac6a67ade7
Thedro Neely <thedroneely@gmail.com>
2018-11-09 23:00:53 -0500

    app/views/index.view: Minor homepage improvements

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -10,59 +10,96 @@
     </div>
     <div class="hero-body">
         <div class="container">
-            <div class="columns">
-                <div class="column is-8 has-text-left">
-                    <h1 class="title"><span>Web<span class="has-text-danger"> Developer</span></span></h1>
-                    <h2 class="subtitle">
-                        Thedro Neely
+            <div class="columns is-centered">
+                <div class="column is-8">
 
+                    <h1 class="title has-text-weight-normal">
+                        <span>
+                            Web
+                            <span class="has-text-danger">
+                                Developer
+                            </span>
+                        </span>
+                    </h1>
+
+                    <h2 class="subtitle has-text-grey-dark has-text-weight-normal">
+                        Thedro Neely
                     </h2>
+
                     <div class="content">
 
-                    <p class="has-text-left has-text-black">
-        My name is Thedro (tee-dro) &mdash; a web developer and self-taught
-        <a href="https://en.wikipedia.org/wiki/Linux">linux and unix-like</a>
-        system administrator.
-        There are many different tools and programming languages I've come across.
-        Welcome to my small part of the web. Here you'll find things I've
-        worked on as well as other interesting discoveries.
+                    <p class="has-text-left has-text-black line__height">
+                        My name is Thedro (tee-dro) &mdash; a web developer and self-taught
+                        <a href="https://en.wikipedia.org/wiki/Linux">linux and unix-like</a>
+                        system administrator.
+                        There are many different tools and programming languages I've come across.
+                        Welcome to my small part of the web. Here you'll find things I've
+                        worked on as well as other interesting discoveries.
                     </p>
 
                     </div>
 
                     <div class="columns">
                         <div class="column">
-                            <h3 class="subtitle has-text-greyer is-marginless">Recent Posts</h3>
-                            <br>
-                            <ul>
+                            <h3 class="heading has-text-black is-marginless is-size-5">
+                                Recent Posts
+                            </h3>
 
-                                <li><a href="/post/mixing-php-into-hugo/">Mixing PHP into Hugo</a></li>
-                                <li><a href="/post/now-dns-pfsense/">Now-DNS and pfSense</a></li>
+                            <br>
+
+                            <ul>
+                                <li class="link__padding">
+                                    <a href="/post/my-sublime-text-setup/">My Sublime Text Setup</a>
+                                </li>
+
+                                <li class="link__padding">
+                                    <a href="/post/mixing-php-into-hugo/">Mixing PHP into Hugo</a>
+                                </li>
+
+                                <li class="link__padding">
+                                    <a href="/post/now-dns-pfsense/">Now-DNS and pfSense</a>
+                                </li>
 
                                 <br>
 
-                                <li><a class="front-frame" href="post/">More posts<span class="icon">➤</span></a></li>
-
+                                <li><a class="front__frame" href="post/">More posts<span class="icon">➤</span></a></li>
                             </ul>
                         </div>
 
                         <div class="column">
-                            <h3 class="subtitle has-text-greyer is-marginless">Recent Projects</h3>
-                            <br>
-                            <ul>
+                            <h3 class="heading has-text-black is-marginless is-size-5">
+                                Recent Projects
+                            </h3>
 
-                                <li><a href="/project/personal-portfolio/">Personal Portfolio</a></li>
-                                <li><a href="/project/voiceover-website/">Voiceover Website</a></li>
+                            <br>
+
+                            <ul>
+                                <li class="link__padding">
+                                    <a href="/project/bulma-resume">Bulma Resume</a>
+                                </li>
+
+                                <li class="link__padding">
+                                    <a href="/project/personal-portfolio/">Personal Portfolio</a>
+                                </li>
+
+                                <li class="link__padding">
+                                    <a href="/project/voiceover-website/">Voiceover Website</a>
+                                </li>
 
                                 <br>
 
-                                <li><a class="front-frame" href="project/">More projects<span class="icon">➤</span></a></li>
+                                <li><a class="front__frame" href="project/">More projects<span class="icon">➤</span></a></li>
                             </ul>
                         </div>
                     </div>
+
+                  <!--   <div>
+                        <img width="200" src="/images/valley.png">
+                    </div> -->
+
                 </div>
 
-                <div class="column is-4 has-text-left">
+               <!--  <div class="column is-4 has-text-left">
 
                     <div class="tabs is-boxed" style="margin-bottom: 0;">
                         <ul>
@@ -82,29 +119,24 @@
 
                     <div style="background-color: #f2f2f2; padding: 10px;border: 1px solid #dbdbdb; min-height: 350px;">
 
-
-
-<div class="card">
-  <header class="card-header">
-    <p class="card-header-title is-size-7">
-      Last Github Commit
-    </p>
-    <a href="#" class="card-header-icon" aria-label="more options">
-    </a>
-  </header>
-  <div class="card-content" style="padding: 0.5rem;">
-    <div class="content is-size-7">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-    </div>
-  </div>
-</div>
-
-
-
+                        <div class="card">
+                          <header class="card-header">
+                            <p class="card-header-title is-size-7">
+                              Last Github Commit
+                            </p>
+                            <a href="#" class="card-header-icon" aria-label="more options">
+                            </a>
+                          </header>
+                          <div class="card-content" style="padding: 0.5rem;">
+                            <div class="content is-size-7">
+                              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                            </div>
+                          </div>
+                        </div>
 
                     </div>
 
-                </div>
+                </div> -->
 
             </div>
         </div>
