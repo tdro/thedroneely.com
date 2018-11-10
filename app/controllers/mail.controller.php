643f4d8b30d5b134ed72e36b8c4637985b31f077
Thedro Neely <thedroneely@gmail.com>
2018-11-09 22:56:40 -0500

    app/controllers/mail.controller: Add FastCGI finish to remove delay on contact form submit.

--- app/controllers/mail.controller.php
+++ app/controllers/mail.controller.php
@@ -1,19 +1,33 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 $form = new Form(
     $name = $_POST['26471'],
     $email = $_POST['26472'],
     $message = $_POST['26478']
 );
 
+// $contact['database']->insert(
+//     'contactform', [
+//     'name' => $form->name,
+//     'email' => $form->email,
+//     'message' => $form->message,
+//     ]
+// );
+
+$validEmail = PHPMailer::validateAddress($email, 'auto');
+
+if ($validEmail === false ) {
+    $mailError = 'Invalid email' . "\r" . '<em>'. $email . '</em>';
+    include '../app/views/mail-error.view.php';
+}
+
+die(var_dump($e));
+
+require '../app/views/mail-sent.view.php';
+
+fastcgi_finish_request();
+
 $form->isSubmit();
-
-// require '../app/views/mail-sent.view.php';
-// fastcgi_finish_request();
-
-$contact['database']->insert(
-    'contactform', [
-    'name' => $form->name,
-    'email' => $form->email,
-    'message' => $form->message,
-    ]
-);
