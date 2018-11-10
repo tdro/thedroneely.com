638f67fec1cb800159bf64bd1af8de50652be96d
Thedro Neely <thedroneely@gmail.com>
2018-11-09 22:52:21 -0500

    app/views/contact: Contact page minor adjustments

--- app/views/contact.view.php
+++ app/views/contact.view.php
@@ -10,80 +10,79 @@
 
     </div>
 
-<div id="contact-vue">
-    <div class="hero-body">
-        <div class="container">
-            <div class="columns">
-                <div class="column is-6 is-offset-3 has-text-left">
-                    <h1 class="title is-3">Contact</h1>
+    <div id="contact-vue">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-centered">
+                    <div class="column is-8">
 
-                    <div class="content">
+                        <article class="content">
 
-                    <p>
-                    Interested in collaborating together?
-                    Complete the form below with basic info about your project or idea.
-                    Please allow 24-48 hours for a response.
-                    </p>
+                            <h1 class="title is-3">Contact</h1>
 
-                    </div>
+                            <p>
+                            Interested in collaborating together?
+                            Complete the form below with basic info about your project or idea.
+                            </p>
 
-                        <form method="post" action="/?sent">
+                            <form method="post" action="/?sent">
 
-                            <div class="field is-horizontal">
-                                <div class="field-body">
-                                    <div class="field is-expanded">
-                                    <label class="label">Name <span class="alert" v-show="!name">*</span></label>
-                                        <div class="field has-addons">
-                                            <p class="control is-expanded">
-                                              <input v-model="name" class="input" type="text" name="26471" placeholder="Jason Response">
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div class="field is-expanded">
-                                        <label class="label">Email <span class="alert" v-show="!email">*</span></label>
+                                <div class="field is-horizontal">
+                                    <div class="field-body">
+                                        <div class="field is-expanded">
+                                        <label class="heading is-size-6">Name <span class="alert" v-show="!name">*</span></label>
                                             <div class="field has-addons">
                                                 <p class="control is-expanded">
-                                                  <input v-model="email" class="input" type="email" name="26472" placeholder="user@domain.com">
+                                                  <input v-model="name" class="input" type="text" name="26471" placeholder="Jason Response">
                                                 </p>
                                             </div>
+                                        </div>
+                                        <div class="field is-expanded">
+                                            <label class="heading is-size-6">Email <span class="alert" v-show="!email">*</span></label>
+                                                <div class="field has-addons">
+                                                    <p class="control is-expanded">
+                                                      <input v-model="email" class="input" type="email" name="26472" placeholder="user@domain.com">
+                                                    </p>
+                                                </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <label class="label">Message <span class="alert" v-show="!message">*</span></label>
+                                <label class="heading is-size-6">Message <span class="alert" v-show="!message">*</span></label>
+                                    <div class="field is-horizontal">
+                                      <div class="field-body">
+                                        <div class="field">
+                                          <div class="control">
+                                            <textarea v-model="message" class="textarea" name="26478" placeholder="How would you describe your project?"></textarea>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+
+                                <br>
+
                                 <div class="field is-horizontal">
                                   <div class="field-body">
                                     <div class="field">
                                       <div class="control">
-                                        <textarea v-model="message" class="textarea" name="26478" placeholder="How would you describe your project?"></textarea>
+                                        <button class="button is-primary contact__button has-text-weight-normal">
+                                          Send message
+                                        </button>
                                       </div>
                                     </div>
                                   </div>
                                 </div>
 
-                            <br>
+                               <input type="checkbox" name="contact" class="form" tabindex="-1" autocomplete="off">
 
-                            <div class="field is-horizontal">
-                              <div class="field-body">
-                                <div class="field">
-                                  <div class="control">
-                                    <button class="button is-primary">
-                                      Send message
-                                    </button>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
+                            </form>
 
-                           <input type="checkbox" name="contact" class="form" tabindex="-1" autocomplete="off">
-
-                        </form>
+                        </article>
 
                     </div>
                 </div>
             </div>
         </div>
-    <!-- <pre>{{ $data | json }} </pre> -->
-</div>
+    </div>
 
 <?php require __DIR__ . '/partials/footer.php'; ?>
