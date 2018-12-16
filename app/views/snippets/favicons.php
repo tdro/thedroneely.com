dcc9e00329fb52f12f31caeb6941f94689b5e08c
Thedro Neely <thedroneely@gmail.com>
2018-12-16 11:33:40 -0500

    app/views/snippets/favicons: Use base64 encoded images

--- app/views/snippets/favicons.php
+++ app/views/snippets/favicons.php
@@ -1,6 +1,6 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAABGdBTUEAALGPC/xhBQAAABhQTFRF////AAAAAAAAAAAAAAAAAAAAAAAAAAAAXGJBvQAAAAd0Uk5TAAKe3ubu+G7lFNYAAABUSURBVCiRYwgvRwElDKj88jJsAkXqaAKKQqgCRQKM6mgCDEJoWhiQlIANRVYCthZZCVYBDC0QBQxggGQtsgDYBIQA1OlIAhieIxwew0fALQ0FJAMAvX3RdZppqNUAAAAASUVORK5CYII=">
+    <link rel="icon" type="image/png" sizes="16x16" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQBAMAAADt3eJSAAAABGdBTUEAALGPC/xhBQAAABhQTFRF////AAAAAAAAAAAAAAAAAAAAAAAAAAAAXGJBvQAAAAd0Uk5TACjn7fT4+0S9dAQAAAA2SURBVAiZY0gvB4MShnIlMKOUoUhQHcoQEIIwyhUZ1VEZICkGBgaIYjADrh1iHqkM11AwCAYAbSsxbLDpk3YAAAAASUVORK5CYII=">
     <link rel="manifest" href="/site.webmanifest">
     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#b91d47">
