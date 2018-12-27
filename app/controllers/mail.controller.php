6152b7394a1ff2211a79dc3521593f3b7deabb6b
Thedro Neely <thedroneely@gmail.com>
2018-12-27 17:18:30 -0500

    app/controllers/mail: Let mail controller commit to database

--- app/controllers/mail.controller.php
+++ app/controllers/mail.controller.php
@@ -9,23 +9,23 @@ $form = new Form(
     $message = $_POST['26478']
 );
 
-// $contact['database']->insert(
-//     'contactform', [
-//     'name' => $form->name,
-//     'email' => $form->email,
-//     'message' => $form->message,
-//     ]
-// );
-
 $validEmail = PHPMailer::validateAddress($email, 'auto');
 
 if ($validEmail === false ) {
     $mailError = 'Invalid email' . "\r" . '<em>'. $email . '</em>';
     include '../app/views/mail-error.view.php';
+    return;
 }
 
 require '../app/views/mail-sent.view.php';
 
 fastcgi_finish_request();
-
 $form->isSubmit();
+
+$contact['database']->insert(
+    'contactform', [
+    'name' => $form->name,
+    'email' => $form->email,
+    'message' => $form->message,
+    ]
+);
