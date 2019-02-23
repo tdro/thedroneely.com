6783137b179f6c49b26976c8416ff5d60779a0a6
Thedro Neely <thedroneely@gmail.com>
2019-02-23 04:53:04 -0500

    app/views/resume: Update webdev resume summary Small syntax changes and additions

--- app/views/resume.view.php
+++ app/views/resume.view.php
@@ -22,211 +22,216 @@
 
 </head>
 
-    <body>
+    <body class="has-background-grey-dark">
 
-    <div class="columns max__height is-gapless">
+    <div class="container is-fullhd">
 
-      <div class="column column__background is-4">
+        <div class="columns max__height is-gapless">
 
-        <section class="section has-text-centered">
+            <div class="column column__background is-4">
 
-            <a href="/resume" class="title has-text-weight-light is-size-4 has-text-white inline__padding underline__hover">Resume</a>
+            <section class="section has-text-centered">
 
-            <br>
-
-            <a href="/contact/" class="title has-text-weight-light is-size-7 has-text-white text__frame">Contact for Full Resume</a>
-
-            <br>
-
-            <h1 class="title has-text-weight-light is-size-3 has-text-white inline__padding">Web <br> Developer</h1>
-
-            <br>
-
-            <p>
-                <a href="/" class="title has-text-weight-light is-size-7 has-text-white white__highlight underline__hover">https://www.thedroneely.com</a>
-            </p>
-
-            <br>
-
-            <p>
-                <a href="#tools" class="title has-text-weight-light is-size-7 has-text-white white__highlight underline__hover">#Tools</a>
-
-                <a href="#projects" class="title has-text-weight-light is-size-7 has-text-white white__highlight underline__hover">#Projects</a>
-
-                <a href="#questions" class="title has-text-weight-light is-size-7 has-text-white white__highlight underline__hover">#Questions</a>
-            </p>
-
-        </section>
-
-      </div>
-
-      <div class="column column__right is-size-6 has-background-white">
-
-            <div id="tools" class="resume__container">
-
-            <div class="resume__break"></div>
-            <div class="resume__break"></div>
-
-            <div class="column resume__column">
-
-                <section>
-
-                    <h1 class="title has-text-weight-light has-text-primary is-4">
-                        Thedro Neely
-                    </h1>
-
-                    <p class="is-size-7">
-                        Hi, My name is Thedro, and I'm a web developer based in The Bahamas.
-                    </p>
-
-                </section>
+                <a href="/resume" class="title has-text-weight-light is-size-4 has-text-white inline__padding underline__hover">Resume</a>
 
                 <br>
 
-                <div class="columns">
+                <a href="/contact/" class="title has-text-weight-light is-size-7 has-text-white text__frame">Contact for Full Resume</a>
 
-                    <div class="column">
+                <br>
 
-                        <section>
-                            <h1 class="heading title has-text-weight-semibold">Stack</h1>
-                        </section>
+                <h1 class="title has-text-weight-light is-size-3 has-text-white inline__padding">Web <br> Developer</h1>
 
-                        <br>
+                <br>
 
-                        <section>
+                <p>
+                    <a href="/" class="title has-text-weight-light is-size-7 has-text-white white__highlight underline__hover">https://www.thedroneely.com</a>
+                </p>
 
-                            <div class="timeline">
+                <br>
 
-                            <div class="timeline-item">
-                            <div class="timeline-marker"></div>
-                            <div class="timeline-content">
-                              <p class="heading">Front End</p>
-                              <p class="is-size-7">HTML &nbsp; CSS &nbsp; JavaScript &nbsp; Bulma</p>
-                              <p class="is-size-7">Bootstrap &nbsp; Vue.js &nbsp; Sass</p>
-                            </div>
-                            </div>
+                <p>
+                    <a href="#tools" class="title has-text-weight-light is-size-7 has-text-white white__highlight underline__hover">#Tools</a>
 
-                            <div class="timeline-item">
-                            <div class="timeline-marker"></div>
-                            <div class="timeline-content">
-                              <p class="heading">Back End</p>
-                              <p class="is-size-7">PHP &nbsp; Python &nbsp; MariaDB &nbsp; MySQL</p>
-                              <p class="is-size-7">PostgreSQL &nbsp; Nginx &nbsp; Apache </p>
-                            </div>
-                            </div>
+                    <a href="#projects" class="title has-text-weight-light is-size-7 has-text-white white__highlight underline__hover">#Projects</a>
 
-                            <div class="timeline-item">
-                            <div class="timeline-marker"></div>
-                            <div class="timeline-content">
-                              <p class="heading">Tooling and Frameworks</p>
-                              <p class="is-size-7">LaTeX &nbsp; Laravel &nbsp; Sublime Text &nbsp; Npm</p>
-                              <p class="is-size-7">Node.js &nbsp; Webpack &nbsp; Gimp</p>
-                            </div>
-                            </div>
+                    <a href="#questions" class="title has-text-weight-light is-size-7 has-text-white white__highlight underline__hover">#Questions</a>
+                </p>
 
-                            <div class="timeline-item">
-                            <div class="timeline-marker"></div>
-                            <div class="timeline-content">
-                              <p class="heading">Version Systems</p>
-                              <p class="is-size-7">Git &nbsp; Gitlab &nbsp; Github</p>
-                            </div>
-                            </div>
+            </section>
 
-                            <div class="timeline-item">
-                            <div class="timeline-marker"></div>
-                            <div class="timeline-content">
-                              <p class="heading">Sys Admin</p>
-                              <p class="is-size-7">Dnsmasq &nbsp; SSH &nbsp; DD-WRT &nbsp; Iptables</p>
-                              <p class="is-size-7">Bash &nbsp; VNC &nbsp; Libvirt &nbsp; pfSense</p>
-                              <p class="is-size-7">RDP &nbsp; Docker &nbsp; Systemd &nbsp; Samba</p>
-                              <p class="is-size-7">CUPS &nbsp; KVM &nbsp; QEMU &nbsp;</p>
-                            </div>
-                            </div>
-
-                            </div>
-
-                        </section>
-
-                    </div>
-
-                    <div class="column">
-
-                         <section id="questions">
-                            <h1 class="heading title has-text-weight-semibold">Questions</h1>
-                        </section>
-
-                        <br>
-
-                        <section>
-
-                            <div class="timeline">
-
-                            <div class="timeline-item">
-                            <div class="timeline-marker"></div>
-                            <div class="timeline-content">
-                                <p class="heading">Are you currently available for work?</p>
-                                <p class="is-size-7">
-                                    Yes, I'm currently available for
-                                    contract work and full-time job
-                                    opportunities.
-                                </p>
-                            </div>
-                            </div>
-
-                            <div class="timeline-item">
-                            <div class="timeline-marker"></div>
-                            <div class="timeline-content">
-                                <p class="heading">How long have you been programming?</p>
-                                <p class="is-size-7">
-                                    For approximately 3 years. I was recently taught PHP by
-                                    a fellow programmer however web application development is not a recent endeavor.
-                                    I've made quite a few websites before this, but mostly in private.
-                                </p>
-                            </div>
-                            </div>
-
-                            </div>
-
-                        </section>
-
-                    </div>
-
-                </div>
             </div>
 
-            <br>
+            <div class="column column__right is-size-6 has-background-white">
 
-            <section id="projects">
-                <h1 class="heading title has-text-weight-semibold">Projects</h1>
-            </section>
+                <div id="tools" class="resume__container">
 
-            <div class="resume__break"></div>
+                <div class="resume__break"></div>
+                <div class="resume__break"></div>
 
-            <section>
-                <div class="tile is-ancestor">
+                <div class="column resume__column">
 
-                    <div class="tile is-parent">
-                        <a href="/">
-                          <img width="203" height="360" src="/images/thedro.png" alt="thedro">
-                        </a>
+                    <section>
+
+                        <h1 class="title has-text-weight-light has-text-primary is-4">
+                            Thedro Neely
+                        </h1>
+
+                        <p class="is-size-7">
+                            Hi, My name is Thedro, and I'm a web developer based in The Bahamas.
+                        </p>
+
+                    </section>
+
+                    <br>
+
+                    <div class="columns">
+
+                        <div class="column">
+
+                            <section>
+                                <h1 class="heading title has-text-weight-semibold">Stack</h1>
+                            </section>
+
+                            <br>
+
+                            <section>
+
+                                <div class="timeline">
+
+                                <div class="timeline-item">
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                  <p class="heading">Front End</p>
+                                  <p class="is-size-7">HTML &nbsp; CSS &nbsp; JavaScript &nbsp; Bulma</p>
+                                  <p class="is-size-7">Bootstrap &nbsp; Vue.js &nbsp; Sass</p>
+                                </div>
+                                </div>
+
+                                <div class="timeline-item">
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                  <p class="heading">Back End</p>
+                                  <p class="is-size-7">PHP &nbsp; Python &nbsp; MariaDB &nbsp; MySQL</p>
+                                  <p class="is-size-7">PostgreSQL &nbsp; Nginx &nbsp; Apache </p>
+                                </div>
+                                </div>
+
+                                <div class="timeline-item">
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                  <p class="heading">Tooling and Frameworks</p>
+                                  <p class="is-size-7">LaTeX &nbsp; Laravel &nbsp; Sublime Text</p>
+                                  <p class="is-size-7">Npm &nbsp; Webpack &nbsp; Gimp &nbsp; Hugo</p>
+                                </div>
+                                </div>
+
+                                <div class="timeline-item">
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                  <p class="heading">Version Systems</p>
+                                  <p class="is-size-7">Git &nbsp; Gitlab &nbsp; Github</p>
+                                </div>
+                                </div>
+
+                                <div class="timeline-item">
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                  <p class="heading">Sys Admin</p>
+                                  <p class="is-size-7">Dnsmasq &nbsp; SSH &nbsp; DD-WRT &nbsp; Iptables</p>
+                                  <p class="is-size-7">Bash &nbsp; VNC &nbsp; Libvirt &nbsp; pfSense</p>
+                                  <p class="is-size-7">RDP &nbsp; Docker &nbsp; Systemd &nbsp; Samba</p>
+                                  <p class="is-size-7">CUPS &nbsp; KVM &nbsp; QEMU &nbsp;</p>
+                                </div>
+                                </div>
+
+                                </div>
+
+                            </section>
+
+                        </div>
+
+                        <div class="column">
+
+                            <section id="questions">
+                                <h1 class="heading title has-text-weight-semibold">Questions</h1>
+                            </section>
+
+                            <br>
+
+                            <section>
+
+                                <div class="timeline">
+
+                                <div class="timeline-item">
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                    <p class="heading">Are you currently available for work?</p>
+                                    <p class="is-size-7">
+                                        Yes, I'm currently available for
+                                        contract work and full-time job
+                                        opportunities.
+                                    </p>
+                                </div>
+                                </div>
+
+                                <div class="timeline-item">
+                                <div class="timeline-marker"></div>
+                                <div class="timeline-content">
+                                    <p class="heading">How long have you been programming?</p>
+                                    <p class="is-size-7">
+                                        For approximately 3 years. I was recently taught PHP by
+                                        a fellow programmer however web application development is not a recent endeavor.
+                                        I've made quite a few websites before this, but mostly in private.
+                                    </p>
+                                </div>
+                                </div>
+
+                                </div>
+
+                            </section>
+
+                        </div>
+
                     </div>
-
-                    <div class="tile is-parent">
-                        <a href="/resume">
-                          <img width="203" height="360" src="/images/resume.png" alt="resume">
-                        </a>
-                    </div>
-
-                    <div class="tile is-parent">
-                        <a href="https://www.edwinmattiacci.com">
-                          <img width="203" height="360" src="/images/edwin.png" alt="edwin">
-                        </a>
-                    </div>
-
                 </div>
-            </section>
+
+                <br>
+
+                <section id="projects">
+                    <h1 class="heading title has-text-weight-semibold">Projects</h1>
+                </section>
+
+                <div class="resume__break"></div>
+
+                <section>
+                    <div class="tile is-ancestor">
+
+                        <div class="tile is-parent">
+                            <a href="/">
+                              <img width="203" height="360" src="/images/thedro.png" alt="thedro">
+                            </a>
+                        </div>
+
+                        <div class="tile is-parent">
+                            <a href="/resume">
+                              <img width="203" height="360" src="/images/resume.png" alt="resume">
+                            </a>
+                        </div>
+
+                        <div class="tile is-parent">
+                            <a href="https://www.edwinmattiacci.com">
+                              <img width="203" height="360" src="/images/edwin.png" alt="edwin">
+                            </a>
+                        </div>
+
+                    </div>
+                </section>
+
+            </div>
+
+          </div>
 
         </div>
 
-      </div>
     </div>
