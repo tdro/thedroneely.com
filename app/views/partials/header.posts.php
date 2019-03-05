1ed375058e409c018c7aba40c330cbcef8e74c4e
Thedro Neely <thedroneely@gmail.com>
2019-03-05 04:41:43 -0500

    app/views/partials/header.posts: Cleanup posts header includes

--- app/views/partials/header.posts.php
+++ app/views/partials/header.posts.php
@@ -1,16 +1,6 @@
+<?php
 
-    <meta name="description" content="Thedro's Portfolio Website Posts">
-    <meta name="keywords" content="Thedro,Neely,Portfolio,Website,Posts">
-    <meta name="author" content="Thedro Neely">
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="robots" content="index,follow">
-
-    <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/bootstrap/Bootstrap.php'; ?>
-
-    <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/snippets/rss.php'; ?>
-
-    <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/snippets/favicons.php'; ?>
-
-    <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/snippets/stylesheets.php'; ?>
+require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/bootstrap/Bootstrap.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/snippets/rss.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/snippets/favicons.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/snippets/stylesheets.php';
