684a5b6a639d3884c5c039a94588d46d49fce40b
Thedro Neely <thedroneely@gmail.com>
2019-03-20 19:22:05 -0400

    app/views/partials/navigator: Wrap long lines and remove mobile nav menu

--- app/views/partials/navigator.php
+++ app/views/partials/navigator.php
@@ -24,56 +24,67 @@
                     </div>
                 </a>
 
-                <a href="/about/" class="navbar-item navbar__icon <?php echo $navigation->isActive('/about/') ?? 'has-text-gray' ?>">
-                    <div class="has-text-centered">
-                    <?php echo file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/..' . '/public/css/fonts/feather-icons/user.svg') ?>
-                    <p class="is-size-7 navbar__icon__align">Profile</p>
-                    </div>
-                </a>
-
                 <a href="/contact/" class="navbar-item navbar__icon <?php echo $navigation->isActive('/contact/') ?? 'has-text-gray' ?>">
                     <div class="has-text-centered">
-                    <?php echo file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/..' . '/public/css/fonts/feather-icons/mail.svg') ?>
+                    <?php echo file_get_contents(
+                        $_SERVER['DOCUMENT_ROOT']
+                        . '/..' . '/public/css/fonts/feather-icons/mail.svg'
+                    ) ?>
                     <p class="is-size-7 navbar__icon__align">Contact</p>
                     </div>
                 </a>
+
+                <div id="navBurgerCompact" class="navbar-burger burger navbar__icon has-text-dark" data-target="navMenu">
+                <a href="javascript:void(0)" class="navbar-item navbar__icon__menu">
+                    <div class="has-text-centered">
+                    <?php echo file_get_contents(
+                        $_SERVER['DOCUMENT_ROOT']
+                        . '/..' . '/public/css/fonts/feather-icons/menu.svg'
+                    ) ?>
+                    <p class="is-size-7 navbar__icon__align">Menu</p>
+                    </div>
+                </a>
+                </div>
 
             </div>
 
             <div class="navbar-brand navbar__tablet__compact">
 
-                <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/partials/navigator.home.php'; ?>
-                <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/partials/navigator.links.php'; ?>
+                <?php require $_SERVER['DOCUMENT_ROOT']
+                . '/..' . '/app/views/partials/navigator.home.php'; ?>
+
+                <?php require $_SERVER['DOCUMENT_ROOT']
+                . '/..' . '/app/views/partials/navigator.links.php'; ?>
 
                 <div id="navBurger" class="navbar-burger burger has-text-dark" data-target="navMenu">
-                  <span></span>
-                  <span></span>
-                  <span></span>
+                <a href="javascript:void(0)" class="navbar-item navbar__icon__menu">
+                    <div class="has-text-centered">
+                    <?php echo file_get_contents(
+                        $_SERVER['DOCUMENT_ROOT']
+                        . '/..' . '/public/css/fonts/feather-icons/menu.svg'
+                    ) ?>
+                    <p class="is-size-7 navbar__icon__align">Menu</p>
+                    </div>
+                </a>
                 </div>
 
             </div>
 
             <div class="navbar-brand is-hidden-touch"></div>
 
-            <div id="navMenu" class="navbar-menu is-hidden-desktop">
-
-                <div class="navbar-end">
-                    <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/partials/navigator.home.php'; ?>
-                    <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/partials/navigator.links.php'; ?>
-                </div>
-
-            </div>
 
             <div class="navbar-menu">
 
                 <div class="columns is-centered has-margin-bottom-none">
 
                     <div class="navbar__left column is-2">
-                        <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/partials/navigator.home.php'; ?>
+                        <?php require $_SERVER['DOCUMENT_ROOT']
+                        . '/..' . '/app/views/partials/navigator.home.php'; ?>
                     </div>
 
                     <div class="navbar__right column is-5">
-                        <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/partials/navigator.links.php'; ?>
+                        <?php require $_SERVER['DOCUMENT_ROOT']
+                        . '/..' . '/app/views/partials/navigator.links.php'; ?>
                     </div>
 
                 </div>
