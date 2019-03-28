a637920cdf8c7dad65758e29b631c6b3439bcc1f
Thedro Neely <thedroneely@gmail.com>
2019-03-28 03:14:20 -0400

    app/views/partials/header: Remove unneeded tags

--- app/views/partials/header.error.php
+++ app/views/partials/header.error.php
@@ -2,9 +2,4 @@
 
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/partials/header.php'; ?>
 
-    <section class="section hero is-fullheight" itemscope itemtype="http://schema.org/HomePage">
-    <div class="hero-head">
-
-    <?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/partials/navigator.php'; ?>
-
-    </div>
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/app/views/partials/navigator.php'; ?>
