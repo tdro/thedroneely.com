f375d1d08faa0b0e98792c34b5a032fa97759af5
Thedro Neely <thedroneely@gmail.com>
2019-04-12 02:00:04 -0400

    app/views/index: Syntax

--- app/views/index.view.php
+++ app/views/index.view.php
@@ -1,8 +1,8 @@
-<?php require __DIR__ . '/partials/header.php'; ?>
+<?php require __DIR__ . '/partials/header.php';?>
 
     <body>
 
-<?php require __DIR__ . '/partials/navigator.php'; ?>
+<?php require __DIR__ . '/partials/navigator.php';?>
 
     <section class="section" itemscope itemtype="http://schema.org/HomePage">
 
@@ -23,7 +23,13 @@
                         In it for the long haul
                     </h2>
 
-                    <img alt="Mountains" width="960" height="480" src="/images/mountains.jpg">
+                    <img alt="Mountains" src="/images/mountains.jpg"
+                    width="<?php echo getimagesize(
+                        $_SERVER['DOCUMENT_ROOT'] . '/images/mountains.jpg'
+                    )[0]; ?>"
+                    height="<?php echo getimagesize(
+                        $_SERVER['DOCUMENT_ROOT'] . '/images/mountains.jpg'
+                    )[1]; ?>" >
 
                     <div class="content">
 
@@ -46,15 +52,18 @@
 
                     <?php
                     $recentPosts = new DOMExtract();
-                    $recentPosts->setSource($_SERVER['DOCUMENT_ROOT'] . '/post/index.html');
+                    $recentPosts->setSource($_SERVER['DOCUMENT_ROOT'] . '/posts/index.html');
                     echo $recentPosts->getInnerHTML('recent-article');
                     ?>
 
                     <a href="/post/"
                         class="front__more is-block has-text-right has-text-centered-mobile">
                         See All Posts
-                        <?php echo file_get_contents($_SERVER['DOCUMENT_ROOT']
-                        . '/..' . '/public/css/fonts/feather-icons/chevron-right.svg') ?>
+                        <?php echo file_get_contents(
+                            $_SERVER['DOCUMENT_ROOT']
+                            . '/..'
+                            . '/public/css/fonts/feather-icons/chevron-right.svg'
+                        ); ?>
                     </a>
 
                     <br>
@@ -63,7 +72,7 @@
 
                     <?php
                     $recentProjects = new DOMExtract();
-                    $recentProjects->setSource($_SERVER['DOCUMENT_ROOT'] . '/project/index.html');
+                    $recentProjects->setSource($_SERVER['DOCUMENT_ROOT'] . '/projects/index.html');
                     echo $recentProjects->getInnerHTML('recent-article');
                     ?>
                     </div>
@@ -71,8 +80,11 @@
                     <a href="/project/"
                     class="front__more is-block has-text-right has-text-centered-mobile">
                         See All Projects
-                        <?php echo file_get_contents($_SERVER['DOCUMENT_ROOT']
-                        . '/..' . '/public/css/fonts/feather-icons/chevron-right.svg') ?>
+                        <?php echo file_get_contents(
+                            $_SERVER['DOCUMENT_ROOT']
+                            . '/..'
+                            . '/public/css/fonts/feather-icons/chevron-right.svg'
+                        ); ?>
                     </a>
 
                     <br>
@@ -83,7 +95,7 @@
 
     </section>
 
-<?php require __DIR__ . '/partials/footer.php'; ?>
+<?php require __DIR__ . '/partials/footer.php';?>
 
   </body>
 </html>
