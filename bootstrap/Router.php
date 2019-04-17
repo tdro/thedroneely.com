b40a524d2514751c248e4d1f7602fdb00ad19908
Thedro Neely <thedroneely@gmail.com>
2019-04-17 09:12:34 -0400

    bootstrap/Router: We die with 404

--- bootstrap/Router.php
+++ bootstrap/Router.php
@@ -35,8 +35,6 @@ class Router
         if (is_array($this->routes[$requestType]) && array_key_exists($uri, $this->routes[$requestType])) {
             return $this->routes[$requestType][$uri];
         }
-        // throw new Exception('No route defined for this URI: "'.$uri.'"');
-
         die(http_response_code(404));
     }
 }
