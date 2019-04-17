e939384c59001d5f3ec22a9819fae2a637a7eb7c
Thedro Neely <thedroneely@gmail.com>
2019-04-16 23:00:04 -0400

    app/views/partials/navigator: Use plural routes for posts and projects

--- app/views/partials/navigator.links.php
+++ app/views/partials/navigator.links.php
@@ -1,5 +1,5 @@
 
-<a href="/post/" class="<?php echo $navigation->isActive('/post/') ?? '' ?>navbar-item navbar__item">Posts</a>
-<a href="/project/" class="<?php echo $navigation->isActive('/project/') ?? '' ?>navbar-item navbar__item">Projects</a>
+<a href="/posts/" class="<?php echo $navigation->isActive('/posts/') ?? '' ?>navbar-item navbar__item">Posts</a>
+<a href="/projects/" class="<?php echo $navigation->isActive('/projects/') ?? '' ?>navbar-item navbar__item">Projects</a>
 <a href="/about/" class="<?php echo $navigation->isActive('/about/') ?? '' ?>navbar-item navbar__item">Profile</a>
 <a href="/contact/" class="<?php echo $navigation->isActive('/contact/') ?? '' ?>navbar-item navbar__item">Contact</a>
