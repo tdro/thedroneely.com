b348e8f87431623bb398eb13d3784157a82339a3
Thedro Neely <thedroneely@gmail.com>
2019-04-30 20:50:34 -0400

    app/model/Navigation: Just use strlen for home page check

--- app/model/Navigation.php
+++ app/model/Navigation.php
@@ -12,7 +12,7 @@ class Navigation
 
      public function isActiveHome()
     {
-        if (substr_count($_SERVER['REQUEST_URI'], '/') === 1) {
+        if (strlen($_SERVER['REQUEST_URI']) === 1) {
             return 'navbar__active ';
         }
         return;
