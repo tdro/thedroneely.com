643b4e048b268da99794261c1afed8326c834a3e
Thedro Neely <thedroneely@gmail.com>
2019-07-25 18:37:22 -0400

    app/views/snippets/stylesheets: Hide comments using CSS and noscript tags in the absence of JavaScript

--- app/views/snippets/stylesheets.php
+++ app/views/snippets/stylesheets.php
@@ -1,12 +1,14 @@
-<noscript>
-    <style>
-        .has-navbar-fixed-top { padding-top: 0 !important; }
-        .is-fixed-top { position: unset !important; }
-    </style>
-</noscript>
-
 <link rel="stylesheet" href="/css/tdro.css">
 
 <?php if ($theme->color() === 'dark') { ?>
 <link rel="stylesheet" href="/css/tdro-dark.css">
 <?php } ?>
+
+<noscript>
+    <style>
+        .has-navbar-fixed-top { padding-top: 0 !important; }
+        .is-fixed-top { position: unset !important; }
+        .posts__more__content { margin-bottom: 0 !important; }
+        .isso-comments-counter, .comments__section { display: none; }
+    </style>
+</noscript>
