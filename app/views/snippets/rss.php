6bd0ac6e79bc2cdca649abceed0982d4e286439e
Thedro Neely <thedroneely@gmail.com>
2019-11-03 01:10:48 -0400

    app/views/snippets/rss: Link RSS feeds directly Move rss.xml to _default to prioritize for Hugo

--- app/views/snippets/rss.php
+++ app/views/snippets/rss.php
@@ -1,1 +1,2 @@
-<link href="/rss.xml" rel="alternate" type="application/rss+xml" title="Thedro Neely"/>
+<link href="/posts/rss.xml" rel="alternate" type="application/rss+xml" title="Posts at Thedro Neely"/>
+<link href="/projects/rss.xml" rel="alternate" type="application/rss+xml" title="Projects at Thedro Neely"/>
