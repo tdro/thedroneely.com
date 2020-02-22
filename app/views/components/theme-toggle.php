da753c1502925961f591853ed868a9afd2a25388
Thedro Neely <thedroneely@gmail.com>
2020-02-21 19:59:34 -0500

    app/views/components/theme-toggle: Hide icons on mobile view

--- app/views/components/theme-toggle.php
+++ app/views/components/theme-toggle.php
@@ -12,7 +12,7 @@
     type="submit"
     value="light"
   >
-    <?php echo featherIcon('sun'); ?>
+    <?php echo featherIcon('sun', 'is-hidden-mobile'); ?>
     <span>Light</span>
   </button>
 
@@ -25,7 +25,7 @@
     type="submit"
     value="dark"
   >
-    <?php echo featherIcon('moon'); ?>
+    <?php echo featherIcon('moon', 'is-hidden-mobile'); ?>
     <span>Dark</span>
   </button>
 
