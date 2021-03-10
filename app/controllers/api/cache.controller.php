af5e135ae3c4493351861142a9f16f26d1b573b2
Thedro Neely <thedroneely@gmail.com>
2021-03-09 19:58:29 -0500

    bootstrap/helpers/cache-bust -> bootstrap/helpers/cache

--- app/controllers/api/cache.controller.php
+++ app/controllers/api/cache.controller.php
@@ -3,5 +3,5 @@
  * Invalidate cache
  */
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo $cacheBust = shell_exec('../bootstrap/helpers/cache-bust');
+    echo $cacheBust = shell_exec('../bootstrap/helpers/cache');
 }
