abbfc1936fdced50d0a62bc45357735b0e7ec42d
Thedro Neely <thedroneely@gmail.com>
2021-03-11 01:39:09 -0500

    app/controllers/api: Show standard errors
    app/controllers/api/cache.controller.php
    app/controllers/api/thumbnails.controller.php

--- app/controllers/api/cache.controller.php
+++ app/controllers/api/cache.controller.php
@@ -3,5 +3,5 @@
  * Invalidate cache
  */
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo $cacheBust = shell_exec('../bootstrap/helpers/cache');
+    echo $cacheBust = shell_exec('../bootstrap/helpers/cache 2>&1');
 }
--- app/controllers/api/thumbnails.controller.php
+++ app/controllers/api/thumbnails.controller.php
@@ -3,5 +3,5 @@
  * Update Thumbnails
  */
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo $updateTumbnails = shell_exec('../bootstrap/helpers/thumbnails');
+    echo $updateTumbnails = shell_exec('../bootstrap/helpers/thumbnails 2>&1');
 }
