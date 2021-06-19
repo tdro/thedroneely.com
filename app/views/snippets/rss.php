e23ca7127c0375625e086078c942c98382bef68a
Thedro Neely <thedroneely@gmail.com>
2021-06-18 20:05:26 -0400

    app/views/snippets/rss: Add abstracts

--- app/views/snippets/rss.php
+++ app/views/snippets/rss.php
@@ -1,3 +1,4 @@
 <link href="/rss.xml" rel="alternate" type="application/rss+xml" title="All at Thedro Neely"/>
 <link href="/posts/rss.xml" rel="alternate" type="application/rss+xml" title="Posts at Thedro Neely"/>
 <link href="/projects/rss.xml" rel="alternate" type="application/rss+xml" title="Projects at Thedro Neely"/>
+<link href="/abstracts/rss.xml" rel="alternate" type="application/rss+xml" title="Abstracts at Thedro Neely"/>
