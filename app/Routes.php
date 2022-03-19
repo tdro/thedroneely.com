1ed73b3b7b88c08d28f57d918963227bf18c72ba
Thedro Neely <thedroneely@gmail.com>
2022-03-19 03:26:22 -0400

    app/Routes: Allow HEAD on resume route

--- app/Routes.php
+++ app/Routes.php
@@ -2,28 +2,22 @@
 /**
  * Public routes
  */
-$router->get('', '../app/controllers/index.controller.php');
-
+$router->get('',        '../app/controllers/index.controller.php');
 $router->get('contact', '../app/controllers/contact.controller.php');
-
-$router->get('resume', '../app/controllers/resume.controller.php');
-
-$router->get('upload', '../app/controllers/upload.controller.php');
+$router->get('resume',  '../app/controllers/resume.controller.php');
+$router->get('upload',  '../app/controllers/upload.controller.php');
 
 
-$router->head('', '../app/controllers/index.controller.php');
-
+$router->head('',        '../app/controllers/index.controller.php');
 $router->head('contact', '../app/controllers/contact.controller.php');
+$router->head('resume',  '../app/controllers/contact.controller.php');
 
 
-$router->post('upload', '../app/controllers/upload.controller.php');
-
+$router->post('upload',  '../app/controllers/upload.controller.php');
 $router->post('contact', '../app/controllers/contact.controller.php');
-
 
 /**
  *  Api routes
  */
 $router->post('api/v1/thumbnails', '../app/controllers/api/thumbnails.controller.php');
-
-$router->post('api/v1/cache', '../app/controllers/api/cache.controller.php');
+$router->post('api/v1/cache',      '../app/controllers/api/cache.controller.php');
