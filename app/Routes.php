1cb3e3c866bbf16f020ed37080d4f3590f90806b
Thedro Neely <thedroneely@gmail.com>
2023-06-05 18:03:11 -0400

    bootstrap/Bootstrap: Clean up

--- app/Routes.php
+++ app/Routes.php
@@ -1,7 +1,11 @@
 <?php
+
+$router = new Router;
+
 /**
  * Public routes
  */
+
 $router->get('',        '../app/controllers/index.controller.php');
 $router->get('contact', '../app/controllers/contact.controller.php');
 $router->get('resume',  '../app/controllers/resume.controller.php');
@@ -17,7 +21,8 @@ $router->post('upload',  '../app/controllers/upload.controller.php');
 $router->post('contact', '../app/controllers/contact.controller.php');
 
 /**
- *  Api routes
+ *  API routes
  */
+
 $router->post('api/v1/thumbnails', '../app/controllers/api/thumbnails.controller.php');
 $router->post('api/v1/cache',      '../app/controllers/api/cache.controller.php');
--- bootstrap/Bootstrap.php
+++ bootstrap/Bootstrap.php
@@ -1,25 +1,13 @@
 <?php
 
-/* composer autoload */
 require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/vendor/autoload.php';
-
-/* source helper functions */
 require $_SERVER['DOCUMENT_ROOT'] . '/..' . '/bootstrap/Helpers.php';
 
-/* source config file */
 $config = include $_SERVER['DOCUMENT_ROOT'] . '/..' . '/config.php';
 
-/* connect database */
 $contact['database'] = new QueryBuilder(Connection::make($config['database']));
 
-/* create new router */
-$router = new Router;
+$navigation = new Navigation;
 
-/* create navigator */
-$navigation = new Navigation();
-
-/* create new theme */
 $theme = new Theme;
-
-/* enable dark and light themes */
 $theme->toggle();
